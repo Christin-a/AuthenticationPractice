fb9e2b12aac1327a395d152c741dc704e1bb1e2a
Christina Armand <christina@Christinas-MacBook-Air.local>
2022-04-16 22:06:49 -0400

    Using bcyrpt

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds =  10;
 
 const app = express();
 
@@ -27,21 +28,52 @@ app.get("/", function(req, res){
 
     res.render("home")
 })
+
+app.get("/register", function(req, res){
+    res.render("register")
+});
+
+app.post("/register", function(req, res){
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User ({
+        email: req.body.username,
+        password: hash
+    });
+    
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("secrets");
+        }
+    });
+});
+
+});
+
+
 app.get("/login", function(req, res){
 
-    res.render("login")
-})
+    res.render("login");
+});
+
+
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
 
     User.findOne({email: username}, function(err, foundUser){
         if(err){
             console.log(err)
         }else{
-            if (foundUser.password === password) {
-                res.render("secrets")
+            if (foundUser) {
+                bcrypt.compare(password, foundUser.password, function(err, results){
+                    if(results === true){
+                        res.render("secrets")                        
+                    }
+                })
             }
         }
 
@@ -50,24 +82,6 @@ app.post("/login", function(req, res){
 });
 
 
-app.get("/register", function(req, res){
-    res.render("register")
-})
-app.post("/register", function(req, res){
-
-    const newUser = new User ({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-
-    newUser.save(function(err){
-        if(err){
-            console.log(err)
-        }else{
-            res.render("secrets")
-        }
-    })
-})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
